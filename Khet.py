2e6f2e7c47cdc3c0a8e9ff4eda8c86a54f645366
Steven <theUberPwnzor@gmail.com>
2012-05-05 08:45:52 -0700

    saturday

--- Khet.py
+++ Khet.py
@@ -1,32 +1,94 @@
 import sys, pygame
+from Khet_Classes import Game_Piece, Pharaoh, Djed, Obelisk, Pyramid
+
+# Read the game piece setup from a text file
+# The three starting configuration in the game will be provided but
+# it is very simple to create your own layout.
+def create_pieces(init_layout):
+    with open("initial_layouts/piece_config_"+str(init_layout)+".txt") as f:
+        temp_piece_list = []
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("#") or line.strip() == "":
+                continue
+            split = line.split(", ")
+            color = split[0]
+            type = split[1]
+            x = split[2]
+            y = split[3]
+            if color == "red":
+                if type == "Pharaoh":
+                    temp_piece_list.append(Pharaoh([x, y], 
+                                                   pygame.image.load("art/r_Pharaoh.png"), 
+                                                   "red"))
+                elif type == "Pyramid":
+                    temp_piece_list.append(Pyramid([x, y], 
+                                                   pygame.image.load("art/r_Pyramid.png"), 
+                                                   int(split[4]),
+                                                   "red"))
+                elif type == "Djed":
+                    temp_piece_list.append(Djed([x, y], 
+                                                pygame.image.load("art/r_Djed.png"),
+                                                split[4],
+                                                "red"))
+                elif type == "Obelisk":
+                    if split[4] == 'True':
+                        temp_piece_list.append(Pharaoh([x, y], 
+                                                       pygame.image.load("art/r_Obelisk.png"), 
+                                                       "red"))
+                        temp_piece_list.append(Pharaoh([x, y], 
+                                                       pygame.image.load("art/r_Obelisk.png"), 
+                                                       "red"))
+                    else:
+                        temp_piece_list.append(Pharaoh([x, y], 
+                                                       pygame.image.load("art/r_Obelisk.png"), 
+                                                       "red"))
+            elif color == 'gray':
+                if type == "Pharaoh":
+                    temp_piece_list.append(Pharaoh([x, y], 
+                                                   pygame.image.load("art/g_Pharaoh.png"), 
+                                                   "gray"))
+                elif type == "Pyramid":
+                    temp_piece_list.append(Pyramid([x, y], 
+                                                   pygame.image.load("art/g_Pyramid.png"), 
+                                                   int(split[4]),
+                                                   "gray"))
+                elif type == "Djed":
+                    temp_piece_list.append(Djed([x, y], 
+                                                pygame.image.load("art/g_Djed.png"),
+                                                split[4],
+                                                "gray"))
+                elif type == "Obelisk":
+                    if split[4] == 'True':
+                        temp_piece_list.append(Pharaoh([x, y], 
+                                                       pygame.image.load("art/g_Obelisk.png"), 
+                                                       "gray"))
+                        temp_piece_list.append(Pharaoh([x, y], 
+                                                       pygame.image.load("art/g_Obelisk.png"), 
+                                                       "gray"))
+                    else:
+                        temp_piece_list.append(Pharaoh([x, y], 
+                                                       pygame.image.load("art/g_Obelisk.png"), 
+                                                       "gray"))
+    return temp_piece_list
     
+# Initialize the PyGame
 pygame.init()
 
 # Set the height and width of the screen
-size = width, height = 583, 477
+size = width, height = 683, 477 #583,477
 screen = pygame.display.set_mode(size)
 
-# Load the art
+# Load the Game Board image
 gameboard = pygame.image.load("art/game_board.png")
-r_pharaoh_piece = pygame.image.load("art/r_Pharaoh.png")
-g_pharaoh_piece = pygame.image.load("art/g_Pharaoh.png")
 
-# Used to manage screen update speed
-clock=pygame.time.Clock()
+# Define colors
+black = (0, 0, 0)
 
-class Pharaoh(object):
-    def __init__(self, loc, img):
-        self._cell_location = loc
-        self._art = img
-        self._alive = True
-    
-    def cell_to_pos(self):
-        x = 28 + (self._cell_location[0] * 53)
-        y = 28 + (self._cell_location[1] * 53)
-        return (x, y)
-
-r_phar = Pharaoh([0,0], r_pharaoh_piece)
-g_phar = Pharaoh([5,5], g_pharaoh_piece)
+# Game Variables
+player_turn = 1
+initial_game_layout = 1 #3 available options
+game_pieces = create_pieces(initial_game_layout)
 
 # -------- Main Program Loop -----------
 done = False
@@ -34,10 +96,13 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        
     
+    # Draw everything
+    screen.fill(black)
     screen.blit(gameboard, gameboard.get_rect())
-    screen.blit(r_phar._art, r_phar.cell_to_pos())
-    screen.blit(g_phar._art, g_phar.cell_to_pos())
+    for p in game_pieces:
+        p.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
--- Khet_Classes.py
+++ Khet_Classes.py
@@ -1,15 +1,163 @@
 import sys, pygame
 
-class Piece():
-    def __init__():
-    _cell_location = [7, 5]
-    _art = []
+class Game_Piece(object):
+    _cell_location = None
+    _base_art = pygame.image.load('art/r_pharaoh.png')
+    _art = pygame.image.load('art/r_pharaoh.png')
+    _alive = True
+    _direction = 1
+    _color = None
     
-    def cell_to_pos():
-        x = 28 + _cell_location * 54
+    def __init__(self, loc, img, col):
+        self._cell_location = loc
+        self._base_art = img
+        self._art = img
+        self._color = col
+    
+    def cell_to_pos(self):
+        x = 28 + (int(self._cell_location[0]) * 53)
+        y = 28 + (int(self._cell_location[1]) * 53)
+        return [x, y]
+    
+    def move(self, x, y):
+        if 0 <= x <= 1 and 0 <= y <= 1:
+            self._cell_location[0] = x
+            self._cell_location[1] = y
+    
+    def draw(self, the_screen):
+        self.rotate_image()
+        the_screen.blit(self._art, self.cell_to_pos())
         
-class Pharaoh(Piece):
-    _art = [pygame.image.load("art/g_Pharaoh.png"),
-            pygame.image.load("art/r_Pharaoh.png")]
+    def rotate(self):
+        pass
+        # Defined in base-classes
+        
+    def rotate_image(self):
+        pass
+        # Defined in the classes that ened it
+        
+    def pass_laser(self):
+        pass
+        # Defined in sub-classes
+        
+class Pharaoh(Game_Piece):
+    def pass_laser(self, indir):
+        #Game over if this piece dies
+        self._alive = False
+        
+class Pyramid(Game_Piece):
+    def __init__(self, loc, img, dir, col):
+        self._cell_location = loc
+        self._base_art = img
+        self._art = img
+        self._color = col
+        self._direction = dir
+        
+    def rotate(self, turn_dir):
+        if turn_dir == 'L':
+            self._direction -= 1
+            if self._direction < 1:
+                self._direction += 4
+        elif turn_dir == 'R':
+            self._direction += 1
+            if self._direction > 4:
+                self._direction -= 4
+        
+    def rotate_image(self):
+        if self._direction == 1:
+            self._art = self._base_art
+        elif self._direction == 2:
+            self._art = pygame.transform.rotate(self._base_art, 180)
+        elif self._direction == 3:
+            self._art = pygame.transform.rotate(self._base_art, 90)
+        elif self._direction == 4:
+            self._art = pygame.transform.rotate(self._base_art, -90)
+        
+    def pass_laser(self, in_dir):
+        if self._direction == 1:
+            if in_dir == 'Right':
+                return 'Down'
+            elif in_dir == 'Down':
+                return 'Right'
+            else:
+                # This piece will die now
+                self._alive = False
+        elif self._direction == 2:
+            if in_dir == 'Left':
+                return 'Up'
+            elif in_dir == 'Up':
+                return 'Left'
+            else:
+                # This piece will die now
+                self._alive = False
+        elif self._direction == 3:
+            if in_dir == 'Up':
+                return 'Right'
+            elif in_dir == 'Right':
+                return 'Up'
+            else:
+                # This piece will die now
+                self._alive = False
+        elif self._direction == 4:
+            if in_dir == 'Down':
+                return 'Right'
+            elif in_dir == 'Right':
+                return 'Down'
+            else:
+                # This piece will die now
+                self._alive = False
+                
+class Djed(Game_Piece):
+    def __init__(self, loc, img, dir, col):
+        self._cell_location = loc
+        self._base_art = img
+        self._art = img
+        self._color = col
+        self._direction = dir
+        
+    def rotate(self):
+        if self._direction == 1:
+            self._direction = 2
+        else:
+            self._direction = 1
             
-    _allowed_moves = [[
+    def pass_laser(self, in_dir):
+        if self._direction == 1:
+            if in_dir == 'Up':
+                return 'Right'
+            elif in_dir == 'Down':
+                return 'Left'
+            elif in_dir == 'Left':
+                return 'Up'
+            elif in_dir == 'Right':
+                return 'Down'
+        elif self._direction == 2:
+            if in_dir == 'Up':
+                return 'Left'
+            elif in_dir == 'Down':
+                return 'Right'
+            elif in_dir == 'Left':
+                return 'Down'
+            elif in_dir == 'Right':
+                return 'Up'
+    
+    # Need to override the move for Djed because it can switch places with pieces
+    def move(self, x, y):
+        if 0 <= x <= 1 and 0 <= y <= 1:
+            self._cell_location[0] = x
+            self._cell_location[1] = y
+        
+        # Need to add code to be able to switch places with pieces
+        
+class Obelisk(Game_Piece):
+    def pass_laser(self, indir):
+        #This piece dies from all sides
+        self._alive = False
+        
+    # Need to override the move for Obelisk because it can stack with other obelisks
+    def move(self, x, y):
+        if 0 <= x <= 1 and 0 <= y <= 1:
+            self._cell_location[0] = x
+            self._cell_location[1] = y
+        
+        # Need to add code to be able to stack
