1240db97c1e3f2269469288d138409b7304a0fe7
Steven <theUberPwnzor@gmail.com>
2012-05-06 05:34:48 -0400

    successfull  night

--- Khet.py
+++ Khet.py
@@ -8,15 +8,14 @@ pygame.init()
 size = width, height = 683, 477 #583,477
 screen = pygame.display.set_mode(size)
 
-
-
 # Define colors
 black = (0, 0, 0)
+white = (255, 255, 255)
 
 # Game Variables
-player_turn = 0 #0 = pregame, 1 = P1 turn, 2 = P2 turn
+player_turn = 1 #0 = pregame, 1 = P1 turn, 2 = P2 turn
 initial_game_layout = 1 #3 available options
-game_board = Game_Board(initial_game_layout)
+game_board = Game_Board(initial_game_layout, screen)
 P1 = Player("P1", [[0,0],"Down"])
 P2 = Player("P2", [[9,7],"Up"])
 p1_win = False
@@ -24,35 +23,85 @@ p2_win = False
 
 # -------- Main Program Loop -----------
 done = False
+screen.fill(black)
+game_board.draw()
 while not done:
-    global game_board
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-		
-        # Get Input for Player Turns & Menu Stuff
-        if player_turn == 0:
-            # This is where any menu screen stuff would go
-            # Right now it just jumps straight into P1 turn
-            player_turn = 1
-        elif player_turn == 1:
-            pass
-        elif player_turn == 2:
-            pass
+    # Getting the input form the user
+    drawing = False
+    surrounding = []
+    curr_piece = None
+    selected = False
+    while not drawing:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                drawing = True
+                player_turn = -1
+                break
+        
+            # Get Input for Player Turns & Menu Stuff
+            if player_turn == 0:
+                # This is where any menu screen stuff would go
+                # Right now it just jumps straight into P1 turn
+               pass
+               
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                m_pos = pygame.mouse.get_pos()
+                m_cell = game_board.coords_to_cell(m_pos)
+                        
+                if selected:
+                    for cell in surrounding:
+                        if m_cell == cell:
+                            game_board.reset_cells(prev_surrounding)
+                            curr_piece.move(cell, game_board)
+                            drawing = True
+                            continue
+                            
+                if game_board.get_cell_status(m_cell) == 'piece':
+                    if player_turn == 1 :
+                        if game_board.get_piece_by_cell(m_cell)._color == 'red':
+                            curr_piece = game_board.get_piece_by_cell(m_cell)
+                            surrounding = curr_piece.get_surrounding_cells(m_cell, game_board)
+                            game_board.reset_cells(prev_surrounding)
+                            game_board.draw_highlight(surrounding, m_cell)
+                            selected = True
+                    elif player_turn == 2:
+                        if game_board.get_piece_by_cell(m_cell)._color == 'gray':
+                            curr_piece = game_board.get_piece_by_cell(m_cell)
+                            surrounding = curr_piece.get_surrounding_cells(m_cell, game_board)
+                            game_board.reset_cells(prev_surrounding)
+                            game_board.draw_highlight(surrounding, m_cell)
+                            selected = True
+            elif event.type == pygame.KEYDOWN and selected and (curr_piece.id == 'pyramid' or curr_piece.id == 'djed'):
+                if event.key == 113:
+                    curr_piece.rotate('L')
+                    game_board.reset_cells(prev_surrounding)
+                    drawing = True
+                elif event.key == 119:
+                    curr_piece.rotate('R')
+                    game_board.reset_cells(prev_surrounding)
+                    drawing = True
+                
+                
+                        
+        # Used to clear highlighted cells on new click                
+        prev_surrounding = surrounding
     
-    # Fire the laser depending on whos turn it is
+    
+    game_board.draw()
+    
+    # The drawing of the lasers
     if player_turn == 1:
         l_path = P1.find_path(game_board)
-        game_board.draw_laser(l_path, screen)
-        done =  True ############## this line temporary for debugging ##############
+        game_board.draw_laser(l_path)
+        player_turn = 2
     elif player_turn == 2:
+        l_path = P2.find_path(game_board)
+        game_board.draw_laser(l_path)
         player_turn = 1
         
-    # Draw everything
-    screen.fill(black)
-    game_board.draw(screen)
-    for p in game_board._game_pieces:
-        p.draw(screen)
-    pygame.display.flip()
+    game_board.draw()
+    
+    
 
 pygame.quit()
--- Khet_Classes.py
+++ Khet_Classes.py
@@ -1,52 +1,90 @@
 import sys, pygame
 
+# Base class for all of the different game pieces
 class Game_Piece(object):
     _cell_location = None
     _base_art = pygame.image.load('art/r_pharaoh.png')
     _art = pygame.image.load('art/r_pharaoh.png')
-    _alive = True
     _direction = 1
     _color = None
     
+    # Constructor
     def __init__(self, loc, img, col):
         self._cell_location = loc
         self._base_art = img
         self._art = img
         self._color = col
     
+    # Convert the [x, y] cell position to the [x, y] coordinates on the screen
     def cell_to_pos(self):
         x = 28 + (int(self._cell_location[0]) * 53)
         y = 28 + (int(self._cell_location[1]) * 53)
         return [x, y]
     
-    def move(self, x, y):
-        if 0 <= x <= 1 and 0 <= y <= 1:
-            self._cell_location[0] = x
-            self._cell_location[1] = y
+    # Move the piece the specifies x and y values
+    def move(self, cell, gameboard):
+        self._cell_location[0] = cell[0]
+        self._cell_location[1] = cell[1]
     
+    # Draw the piece to the screen, called by the main program loop
     def draw(self, the_screen):
         self.adjust_image()
         the_screen.blit(self._art, self.cell_to_pos())
-        
-    def rotate(self):
+        pygame.display.flip()
+    
+    # Rotate the piece
+    def rotate(self, turn_dir):
         pass
         # Defined in base-classes
         
+    # Apply the correct image and image rotation
     def adjust_image(self):
         pass
         # Defined in the classes that ened it
+    
+    # Return a list of the valid surrounding cells
+    # Need to override in Djed class and Obelisk class
+    def get_surrounding_cells(self, cell_pos, gameboard):
+        cell_list = []
         
+        x = cell_pos[0]
+        y = cell_pos[1]
+        
+        if x > 0:
+            cell_list.append([x - 1, y])
+        if x < 9:
+            cell_list.append([x + 1, y])
+        if y > 0:
+            cell_list.append([x, y - 1])
+        if y < 7:
+            cell_list.append([x, y + 1])
+        if x > 0 and y > 0:
+            cell_list.append([x - 1, y - 1])
+        if x < 9 and y < 7:
+            cell_list.append([x + 1, y + 1])
+        if y > 0 and x < 9:
+            cell_list.append([x + 1, y - 1])
+        if y < 7 and x > 0:
+            cell_list.append([x - 1, y + 1])
+        
+        new_list = [cell for cell in cell_list if not gameboard.get_piece_by_cell(cell)]
+        
+        return new_list
+    
+    # Receives a direction and returns the direction the laser would reflect.
+    # Returns 'die' if it kills the piece
     def pass_laser(self, indir):
         pass
         # Defined in sub-classes
         
 class Pharaoh(Game_Piece):
+    id = 'pharaoh'
     def pass_laser(self, indir):
         #Game over if this piece dies
-        self._alive = False
         return 'die'
         
 class Pyramid(Game_Piece):
+    id = 'pyramid'
     def __init__(self, loc, img, dir, col):
         self._cell_location = loc
         self._base_art = img
@@ -68,11 +106,11 @@ class Pyramid(Game_Piece):
         if self._direction == 1:
             self._art = self._base_art
         elif self._direction == 2:
-            self._art = pygame.transform.rotate(self._base_art, 180)
-        elif self._direction == 3:
-            self._art = pygame.transform.rotate(self._base_art, 90)
-        elif self._direction == 4:
             self._art = pygame.transform.rotate(self._base_art, -90)
+        elif self._direction == 3:
+            self._art = pygame.transform.rotate(self._base_art, 180)
+        elif self._direction == 4:
+            self._art = pygame.transform.rotate(self._base_art, 90)
         
     def pass_laser(self, in_dir):
         if self._direction == 1:
@@ -82,37 +120,34 @@ class Pyramid(Game_Piece):
                 return 'Right'
             else:
                 # This piece will die now
-                self._alive = False
                 return 'die'
-        elif self._direction == 2:
+        elif self._direction == 3:
             if in_dir == 'Right':
                 return 'Up'
             elif in_dir == 'Down':
                 return 'Left'
             else:
                 # This piece will die now
-                self._alive = False
                 return 'die'
-        elif self._direction == 3:
+        elif self._direction == 4:
             if in_dir == 'Down':
                 return 'Right'
             elif in_dir == 'Left':
                 return 'Up'
             else:
                 # This piece will die now
-                self._alive = False
                 return 'die'
-        elif self._direction == 4:
+        elif self._direction == 2:
             if in_dir == 'Up':
-                return 'Right'
+                return 'Left'
             elif in_dir == 'Right':
                 return 'Down'
             else:
                 # This piece will die now
-                self._alive = False
                 return 'die'
                 
 class Djed(Game_Piece):
+    id = 'djed'
     def __init__(self, loc, img, dir, col):
         self._cell_location = loc
         self._base_art = img
@@ -120,7 +155,7 @@ class Djed(Game_Piece):
         self._color = col
         self._direction = dir
         
-    def rotate(self):
+    def rotate(self, turn_dir):
         if self._direction == 1:
             self._direction = 2
         else:
@@ -153,14 +188,43 @@ class Djed(Game_Piece):
                 return 'Up'
     
     # Need to override the move for Djed because it can switch places with pieces
-    def move(self, x, y):
-        if 0 <= x <= 1 and 0 <= y <= 1:
-            self._cell_location[0] = x
-            self._cell_location[1] = y
+    def move(self, cell, gameboard):
+        self._cell_location[0] = cell[0]
+        self._cell_location[1] = cell[1]
+                
         
         # Need to add code to be able to switch places with pieces
+    
+    # Need to modify 
+    def get_surrounding_cells(self, cell_pos, gameboard):
+        cell_list = []
+        
+        x = cell_pos[0]
+        y = cell_pos[1]
+        
+        if x > 0:
+            cell_list.append([x - 1, y])
+        if x < 9:
+            cell_list.append([x + 1, y])
+        if y > 0:
+            cell_list.append([x, y - 1])
+        if y < 7:
+            cell_list.append([x, y + 1])
+        if x > 0 and y > 0:
+            cell_list.append([x - 1, y - 1])
+        if x < 9 and y < 7:
+            cell_list.append([x + 1, y + 1])
+        if y > 0 and x < 9:
+            cell_list.append([x + 1, y - 1])
+        if y < 7 and x > 0:
+            cell_list.append([x - 1, y + 1])
+        
+        new_list = [cell for cell in cell_list if not gameboard.get_piece_by_cell(cell)]
+        
+        return new_list  
         
 class Obelisk(Game_Piece):
+    id = 'obelisk'
     _base_art_stacked = pygame.image.load("art/g_Pyramid.png")
     _Stacked = False;
     
@@ -174,7 +238,6 @@ class Obelisk(Game_Piece):
         
     def pass_laser(self, indir):
         #This piece dies from all sides
-        self._alive = False
         return 'die'
     
     def adjust_image(self):
@@ -184,26 +247,80 @@ class Obelisk(Game_Piece):
             self._art = self._base_art
             
     # Need to override the move for Obelisk because it can stack with other obelisks
-    def move(self, x, y):
-        if 0 <= x <= 1 and 0 <= y <= 1:
-            self._cell_location[0] = x
-            self._cell_location[1] = y
+    def move(self, cell, gameboard):
+        old_loc = [self._cell_location[0], self._cell_location[1]]
         
-        # Need to add code to be able to stack
-
+        if gameboard.get_cell_status(cell) == 'piece':
+            piece = gameboard.get_piece_by_cell(cell)
+            if piece.id == 'obelisk' and piece._Stacked == False:
+                piece._Stacked = True
+            self._Stacked = True
+            
+        self._cell_location = cell
+        
+        if gameboard.get_cell_status(old_loc) == 'piece':
+            piece = gameboard.get_piece_by_cell(old_loc)
+            if piece.id == 'obelisk':
+                piece._Stacked = False
+                piece.draw(gameboard.the_screen)
+            self._Stacked = False
+        
+        
+    def get_surrounding_cells(self, cell_pos, gameboard):
+        cell_list = []
+        
+        x = cell_pos[0]
+        y = cell_pos[1]
+        
+        if x > 0:
+            cell_list.append([x - 1, y])
+        if x < 9:
+            cell_list.append([x + 1, y])
+        if y > 0:
+            cell_list.append([x, y - 1])
+        if y < 7:
+            cell_list.append([x, y + 1])
+        if x > 0 and y > 0:
+            cell_list.append([x - 1, y - 1])
+        if x < 9 and y < 7:
+            cell_list.append([x + 1, y + 1])
+        if y > 0 and x < 9:
+            cell_list.append([x + 1, y - 1])
+        if y < 7 and x > 0:
+            cell_list.append([x - 1, y + 1])
+        
+        new_list = [cell for cell in cell_list if not gameboard.get_piece_by_cell(cell) or 
+                                                    (gameboard.get_piece_by_cell(cell).id == 'obelisk' 
+                                                     and not gameboard.get_piece_by_cell(cell)._Stacked)]
+        
+        return new_list
+        
 class Game_Board(object):
     # Load the Game Board image
     _art = pygame.image.load("art/game_board.png")
+    _cell_art = [pygame.image.load("art/cell_empty.png"),
+                 pygame.image.load("art/cell_red.png"),
+                 pygame.image.load("art/cell_gray.png")]
     _initial_layout = 1
     _game_pieces = []
+    _current_highlighted_cell = None
+    the_screen = None
     
-    def __init__(self, init_layout):
+    # Constructor
+    def __init__(self, init_layout, screen):
         _initial_layout = init_layout
         self.create_pieces()
+        self.the_screen = screen
     
-    def draw(self, the_screen):
-        the_screen.blit(self._art, self._art.get_rect())
+    # Draw the game board image to the screen
+    def draw(self):
+        self.the_screen.blit(self._art, self._art.get_rect())
+        for p in self._game_pieces:
+            p.draw(self.the_screen)
+        pygame.display.flip()
     
+    # Return the status of a cell...
+    # Possible Values: "piece", "wall", or "empty"
     def get_cell_status(self, cell_pos):
         for piece in self._game_pieces:
             if piece._cell_location == cell_pos:
@@ -213,23 +330,108 @@ class Game_Board(object):
             if cell_pos[1] < 0 or cell_pos[1] > 7:
                 return 'wall'
                 
-        return 'straight'
-        
+        return 'empty'
+    
+    # Returns the piece located at a specified cell location
     def get_piece_by_cell(self, cell_pos):
         for piece in self._game_pieces:
             if piece._cell_location == cell_pos:
                 return piece
         return False
+    
+    # Returns the center coordinates of the cell location
+    def get_center_by_cell(self, cell_pos):
+        center_x = 52 + cell_pos[0] * 53
+        center_y = 52 + cell_pos[1] * 53
         
-    def draw_laser(self, l_path, the_screen):
+        return [center_x, center_y]
+        
+    # Convert the [x, y] cell position to the [x, y] coordinates on the screen
+    def coords_to_cell(self, coords):
+        x = int(coords[0]-28) / 53
+        y = int(coords[1]-28) / 53
+        return [x, y]
+        
+    # Convert the [x, y] coords to the [x, y] cell position on the screen
+    def cell_to_coords(self, cell):
+        x = 28 + (int(cell[0]) * 53)
+        y = 28 + (int(cell[1]) * 53)
+        return [x, y]
+        
+    # Draw the transparent highlights around valid cells when player clicks a piece
+    def draw_highlight(self, cells, center_cell):
+        hl_img = pygame.image.load("art/hl.png")
+        h2_img = pygame.image.load("art/h2.png")
+        for cell in cells:
+            self.the_screen.blit(hl_img, self.cell_to_coords(cell))
+        self.the_screen.blit(h2_img, self.cell_to_coords(center_cell))
+        pygame.display.flip()
+        self._current_highlighted_cell = center_cell
+    
+    # Remove highlighting from cells
+    def reset_cells(self, cells):
+        cell_list = cells
+        
+        if not self._current_highlighted_cell == None:
+                piece = self.get_piece_by_cell(self._current_highlighted_cell)
+                cell_list.append(piece._cell_location)
+                
+        for cell in cell_list:
+            if self.get_cell_status(cell) == 'empty':
+                # Redraw the empty cell
+                if cell[0] == 0 or (cell[0] == 1 and cell[1] == 0) or (cell[0] == 1 and cell[1] == 7):
+                    # Empty red cell
+                    self.the_screen.blit(self._cell_art[1], self.cell_to_coords(cell))
+                elif cell[0] == 9 or (cell[0] == 8 and cell[1] == 0) or (cell[0] == 8 and cell[1] == 7):
+                    # Empty gray cell
+                    self.the_screen.blit(self._cell_art[2], self.cell_to_coords(cell))
+                else:
+                    # Empty cell
+                    self.the_screen.blit(self._cell_art[0], self.cell_to_coords(cell))
+            elif self.get_cell_status(cell) == 'piece':
+                self.the_screen.blit(self.get_piece_by_cell(cell)._art, self.cell_to_coords(cell))
+        pygame.display.flip()
+        self._current_highlighted_cell = None
+                
+    # Draw the laser beams
+    def draw_laser(self, l_path):
         # Define the line color...Change it with (R,G,B) values
         red = (255, 0, 0)
-        for path_point in l_path:
-            #do stuff
-            pass
         
+        path_point_1 = l_path[0][0]
+        indir = l_path[0][1]
+        path_point_center_1 = self.get_center_by_cell(path_point_1)
+        if indir == 'Up':
+            path_point_center_1[1] += 24
+        elif indir == 'Down':
+            path_point_center_1[1] -= 24
         
-    
+        for i in xrange(len(l_path)-2):
+            path_point_2 = l_path[i+1][0]
+            path_point_center_2 = self.get_center_by_cell(path_point_2)
+            
+            pygame.draw.line(self.the_screen, red, path_point_center_1, path_point_center_2, 2)
+            pygame.display.flip()
+            pygame.time.wait(200)
+            
+            path_point_1 = path_point_2
+            path_point_center_1 = self.get_center_by_cell(path_point_1)
+        
+        path_point_2 = l_path[len(l_path)-1][0]
+        path_point_center_2 = self.get_center_by_cell(path_point_2)
+        if l_path[len(l_path)-2][1] == 'Up':
+            path_point_center_2[1] += 24
+        elif l_path[len(l_path)-2][1] == 'Down':
+            path_point_center_2[1] -= 24
+        elif l_path[len(l_path)-2][1] == 'Left':
+            path_point_center_2[0] += 24
+        elif l_path[len(l_path)-2][1] == 'Right':
+            path_point_center_2[0] -= 24
+            
+        pygame.draw.line(self.the_screen, red, path_point_center_1, path_point_center_2, 2)
+        pygame.display.flip()
+        pygame.time.wait(200)
+            
     # Read the game piece setup from a text file
     # The three starting configuration in the game will be provided but
     # it is very simple to create your own layout.
@@ -317,13 +519,12 @@ class Player(object):
     name = ""
     laser_movement = [] #[[0,0],"Down"] where [0,0] is the next cell the laser will hit and "Down" is the direction it enters that cell
 	
+    # Constructor
     def __init__(self, name, laser_movement):
         self.name = name
         self.laser_movement = laser_movement
 	
-	# Return "Game_Over_Win" if killed other players Pharaoh
-	# Return "Game_Over_Lose if killed your own Pharaoh (i.e. you're stupid.)
-	# Else, Return ""
+	# Find the path of the players laser beam
     def find_path(self, gameboard):
         pieces = gameboard._game_pieces
         l_path = []
@@ -356,6 +557,17 @@ class Player(object):
                 # The direction that this piece forwards the laser
                 forward_dir = curr_piece.pass_laser(curr_dir)
                 if forward_dir == 'die':
+                    if curr_piece.id == 'pharaoh':
+                        if curr_piece._color == 'red':
+                            p1_win = True
+                        elif curr_piece._color == 'gray':
+                            p2_win = True
+                        pieces.remove(curr_piece)
+                    else:
+                        pieces.remove(curr_piece)
+                        if curr_piece.id == 'obelisk' and gameboard.get_piece_by_cell(next_cell):
+                            gameboard.get_piece_by_cell(next_cell)._Stacked = False
+                            
                     l_path.append([next_cell, 'Terminate'])
                     i = -1
                 else:
